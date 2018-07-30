e1340c99d7bfa8a752195698c5fcac899a61cb85
Antonio Cuni <anto.cuni@gmail.com>
2018-07-31 01:29:18 +0200

    start a class to assemble a function

--- jit.py
+++ jit.py
@@ -3,8 +3,9 @@ from cffi import FFI
 ffi = FFI()
 ffi.cdef("typedef double (*d2_d_t)(double, double);")
 
-class CompiledFunction(object):
+class CompiledFunction:
 
+    # XXX: for now we assume that the signature is (double, double) -> double
     def __init__(self, code):
         self.map = mmap.mmap(-1, len(code), mmap.MAP_PRIVATE,
                              mmap.PROT_READ | mmap.PROT_WRITE | mmap.PROT_EXEC)
@@ -13,3 +14,20 @@ class CompiledFunction(object):
 
     def __call__(self, *args):
         return self.fptr(*args)
+
+
+class FunctionAssembler:
+
+    def __init__(self):
+        self.nargs = None
+        self.locals = {} # varname -> offset
+        self._cur_offset = -0x08 # we start with the return address on the stack
+
+    def var_offset(self, varname):
+        try:
+            return self.locals[varname]
+        except KeyError:
+            pass
+        res = self.locals[varname] = self._cur_offset
+        self._cur_offset -= 8
+        return res
--- test_jit.py
+++ test_jit.py
@@ -5,3 +5,12 @@ def test_CompliedFunction():
     p = jit.CompiledFunction(code)
     assert p.fptr(12.34, 56.78) == 12.34 + 56.78
     assert p(12.34, 56.78) == 12.34 + 56.78
+
+class TestFunctionAssembler:
+
+    def test_var_offset(self):
+        fasm = jit.FunctionAssembler()
+        assert fasm.var_offset('a') == -0x08
+        assert fasm.var_offset('b') == -0x10
+        assert fasm.var_offset('c') == -0x18
+        assert fasm.var_offset('a') == -0x08
