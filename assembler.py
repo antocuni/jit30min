ee2d0d992c7e1f2997b37e518ba408c691fbf9d4
Antonio Cuni <anto.cuni@gmail.com>
2019-04-11 00:18:30 +0200

    start to move FunctionAssembler in its own file: it is meant to be a thin wrapper around pechpy with a nicer API, to be used as a starting point during the talk

---
+++ assembler.py
@@ -0,0 +1,43 @@
+import peachpy
+from peachpy import Argument, double_
+from peachpy import x86_64
+# workaround because peachpy forget to expose rsp
+x86_64.rsp = peachpy.x86_64.registers.rsp
+
+
+class FunctionAssembler:
+    def __init__(self, name, argnames):
+        self.name = name
+        args = [Argument(double_, name=name) for name in argnames]
+        self._peachpy_fn = x86_64.Function(name, args, double_)
+
+    def __getattr__(self, name):
+        obj = getattr(x86_64, name)
+        if type(obj) is type and issubclass(obj, x86_64.instructions.Instruction):
+            instr = obj
+            def emit(*args):
+                self._peachpy_fn.add_instruction(instr(*args))
+            return emit
+        else:
+            return obj
+
+    def pushsd(self, reg):
+        self.SUB(self.rsp, 16)
+        self.MOVSD(self.qword[self.rsp], reg)
+
+    def popsd(self, reg):
+        self.MOVSD(reg, self.qword[self.rsp])
+        self.ADD(self.rsp, 16)
+
+    def _encode(self):
+        abi_func = self._peachpy_fn.finalize(x86_64.abi.detect())
+        return abi_func.encode()
+
+    def compile(self):
+        encoded_func = self._encode()
+        #print(encoded_func.format())
+        return encoded_func.code_section.content
+
+    def _as_pyfunc(self):
+        encoded_func = self._encode()
+        return encoded_func.load()
---
+++ test_assembler.py
@@ -0,0 +1,23 @@
+from assembler import FunctionAssembler
+from peachpy import x86_64
+
+class TestFunctionAssembler:
+
+    def test_getattr(self):
+        asm = FunctionAssembler('foo', [])
+        assert asm.xmm0 is x86_64.xmm0
+        assert asm.rsp is x86_64.rsp
+        assert asm.qword is x86_64.qword
+
+    def test_opcode(self):
+        asm = FunctionAssembler('foo', [])
+        asm.ADDSD(asm.xmm0, asm.xmm1)
+        assert len(asm._peachpy_fn._instructions) == 1
+        assert asm._peachpy_fn._instructions[0].__class__.__name__ == 'ADDSD'
+
+    def test_encode(self):
+        asm = FunctionAssembler('foo', ['a', 'b'])
+        asm.ADDSD(asm.xmm0, asm.xmm1)
+        asm.RET()
+        pyfn = asm._as_pyfunc()
+        assert pyfn(3, 4) == 7
